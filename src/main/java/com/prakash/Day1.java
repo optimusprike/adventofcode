767d2d5692cbd6f065e608ec6725f6d652f2866b
Prakash Pudhucode <optimusprike@users.noreply.github.com>
2025-12-01 18:12:38 -0500

    day 1

---
+++ src/main/java/com/prakash/Day1.java
@@ -0,0 +1,78 @@
+package com.prakash;
+
+import io.vavr.control.Try;
+
+import java.util.Arrays;
+
+public class Day1 {
+    private int start;
+    private int result;
+
+    public Day1() {
+        this.start = 50;
+        this.result = 0;
+    }
+
+    public void solve(String fileName) {
+        var list = Try.of(() ->
+                getClass().getClassLoader().getResourceAsStream(fileName)
+        ).recover(Exception.class,
+                ex -> null
+        ).mapTry(row ->
+                new String(row.readAllBytes())
+        ).toJavaOptional().map(input ->
+                Arrays.asList(input.split("\n"))
+        ).get();
+
+        this.start = 50;
+        this.result = 0;
+
+        list.forEach(this::processPart1);
+        System.out.println("part1: " + result);
+
+        this.start = 50;
+        this.result = 0;
+
+        list.forEach(this::processPart2);
+        System.out.println("part2 my way: " + result);
+    }
+
+    private void processPart1(String move) {
+        var direction = move.substring(0, 1);
+        var amount = Integer.parseInt(move.substring(1));
+        amount %= 100;
+        var newPos = direction.equals("L") ? start - amount : start + amount;
+        if (newPos < 0 && start != 0) {
+            newPos += 100;
+        }
+        if (newPos > 100 && start != 0) {
+            newPos -= 100;
+        }
+        if (newPos == 0 || newPos == 100) {
+            newPos = 0;
+            this.result++;
+        }
+        start = newPos;
+    }
+
+    private void processPart2(String move) {
+        var direction = move.substring(0, 1);
+        var amount = Integer.parseInt(move.substring(1));
+        this.result += (amount / 100);
+        amount %= 100;
+        var newPos = direction.equals("L") ? start - amount : start + amount;
+        if (newPos < 0) {
+            newPos += 100;
+            if (start != 0) this.result++;
+        }
+        if (newPos > 100) {
+            newPos -= 100;
+            if (start != 0) this.result++;
+        }
+        if (newPos == 0 || newPos == 100) {
+            newPos = 0;
+            if (start != 0) this.result++;
+        }
+        start = newPos;
+    }
+}
---
+++ src/main/java/com/prakash/Main.java
@@ -0,0 +1,10 @@
+package com.prakash;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Day1 day1 = new Day1();
+
+        day1.solve("input.txt");
+    }
+}
