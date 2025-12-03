ada3b6455ee58e0302e1ed5db096f641a5d12a71
Prakash Pudhucode <optimusprike@users.noreply.github.com>
2025-12-03 08:51:30 -0500

    wip

--- src/main/java/com/prakash/Day2.java
+++ src/main/java/com/prakash/Day2.java
@@ -23,8 +23,166 @@ public class Day2 {
                 Arrays.asList(input.split(","))
         ).get();
 
-        list.forEach(this::part1);
+        // list.forEach(this::part1);
+        list.forEach(this::p1);
         System.out.println("part1: " + result);
+        result = 0;
+        list.forEach(this::p1v2);
+        System.out.println("p1v2: " + result);
+
+        result = 0;
+        list.forEach(this::p2);
+        System.out.println("part2: " + result);
+    }
+
+    public void p1v2(String range) {
+        String[] split = range.split("-");
+        var leftStr = split[0];
+        var rightStr = split[1].split("\n")[0];
+        var len = leftStr.length();
+        if (len % 2 == 1 && rightStr.length() == len) {
+            return;
+        }
+
+        var left = Long.valueOf(leftStr);
+        var right = Long.valueOf(rightStr);
+
+        for (; left <= right; left++) {
+            if(validTwo(String.valueOf(left))) {
+                this.result += left;
+            }
+        }
+    }
+
+    public void p1(String range) {
+        String[] split = range.split("-");
+        var leftStr = split[0];
+        var rightStr = split[1].split("\n")[0];
+        var len = leftStr.length();
+        if (len % 2 == 1 && rightStr.length() == len) {
+            return;
+        }
+
+        var left = Long.valueOf(leftStr);
+        var right = Long.valueOf(rightStr);
+
+        for (; left <= right; left++) {
+            var str = Long.toString(left);
+            if (str.length() % 2 == 1) continue;
+            var mid = str.length() / 2;
+            if (str.substring(0, mid).equals(str.substring(mid))) this.result += left;
+        }
+    }
+
+    public void p2(String range) {
+        String[] split = range.split("-");
+        var leftStr = split[0];
+        var rightStr = split[1].split("\n")[0];
+        var len = leftStr.length();
+        if (len % 2 == 1 && rightStr.length() == len) {
+            return;
+        }
+
+        var left = Long.valueOf(leftStr);
+        var right = Long.valueOf(rightStr);
+
+        for (; left <= right; left++) {
+            var str = String.valueOf(left);
+            if(validTwo(str) || validThree(str) || validFive(str) || validSeven(str) || validEleven(str) || validThirteen(str)) {
+                this.result += left;
+            }
+
+            System.out.println("result: " + result);
+        }
+
+    }
+
+    public boolean validTwo(String str) {
+        if (str.length() % 2 != 0) return false;
+
+        var len = str.length();
+        var divisor = len / 2;
+
+        return str.substring(0, divisor).equals(str.substring(divisor));
+    }
+
+    public boolean validThree(String str) {
+        if (str.length() % 3 != 0) return false;
+
+        var len = str.length();
+        var divisor = len / 3;
+        var num = Long.parseLong(str);
+
+        var lastThird = (long) (num % Math.pow(10, divisor));
+        var remain = (long)Math.floor(num / Math.pow(10, divisor));
+        var middleThird = (long) (remain % Math.pow(10, divisor));
+        var frontThird = (long) Math.floor(remain / Math.pow(10, divisor));
+
+        return frontThird == middleThird && middleThird == lastThird;
+    }
+
+    public boolean validFive(String str) {
+        if (str.length() % 5 != 0) return false;
+
+        var len = str.length();
+        var divisor = len / 5;
+        var num = Long.parseLong(str);
+
+        var lastFifth = (long) (num % Math.pow(10, divisor));
+        var firstFifth = (long) Math.floor(num / Math.pow(10, len - divisor));
+
+        var middle = (long) Math.floor((num - (firstFifth * (Math.pow(10, len - divisor))) - lastFifth) / Math.pow(10, divisor));
+        var middleBack = (long) (middle % Math.pow(10, divisor));
+
+        return firstFifth == lastFifth && firstFifth == middleBack && validThree(String.valueOf(middle));
+    }
+
+    public boolean validSeven(String str) {
+        if (str.length() % 7 != 0) return false;
+
+        var len = str.length();
+        var divisor = len / 7;
+        var num = Long.parseLong(str);
+
+        var lastSeventh = (long) (num % Math.pow(10, divisor));
+        var firstSeventh = (long) Math.floor(num / Math.pow(10, len - divisor));
+
+        var middle = (long) Math.floor((num - (firstSeventh * (Math.pow(10, len - divisor))) - lastSeventh) / Math.pow(10, divisor));
+        var middleBackSeventh = (long) (middle % Math.pow(10, divisor));
+
+        return firstSeventh == lastSeventh && firstSeventh == middleBackSeventh && validFive(String.valueOf(middle));
+    }
+
+    public boolean validEleven(String str) {
+        if (str.length() % 11 != 0) return false;
+
+        var len = str.length();
+        var divisor = len / 11;
+        var num = Long.parseLong(str);
+
+        var lastEleventh = (long) (num % Math.pow(10, divisor));
+        var firstEleventh = (long) Math.floor(num / Math.pow(10, len - divisor));
+
+        var middle = (long) Math.floor((num - (firstEleventh * (Math.pow(10, len - divisor))) - lastEleventh) / Math.pow(10, divisor));
+        var middleBackEleventh = (long) (middle % Math.pow(10, divisor));
+
+        return firstEleventh == lastEleventh && firstEleventh == middleBackEleventh && validThree(String.valueOf(middle));
+    }
+
+    public boolean validThirteen(String str) {
+        if (str.length() % 13 != 0) return false;
+
+        var len = str.length();
+        var divisor = len / 13;
+        var num = Long.parseLong(str);
+
+        var lastThirteenth = (long) (num % Math.pow(10, divisor));
+        var firstThirteenth = (long) Math.floor(num / Math.pow(10, len - divisor));
+
+        var middle = (long) Math.floor((num - (firstThirteenth * (Math.pow(10, len - divisor))) - lastThirteenth) / Math.pow(10, divisor));
+        var middleBackThirteenth = (long) (middle % Math.pow(10, divisor));
+
+        return firstThirteenth == lastThirteenth && firstThirteenth == middleBackThirteenth && validEleven(String.valueOf(middle));
     }
 
     public void part1(String range) {
@@ -58,20 +216,5 @@ public class Day2 {
             combineStr = leftLeftStr + leftLeftStr;
             combine = Long.parseLong(combineStr);
         }
-//        var leftRightStr = leftStr.substring(mid);
-//        var leftRight = Long.parseLong(leftRightStr);
-//
-//
-//
-//        var combine = leftLeftStr + leftLeftStr;
-//
-//
-//        while(left <= right) {
-//            var str = String.valueOf(left);
-//            if(str.length() % 2 == 1) continue;
-//            var mid = str.length() / 2;
-//            if(str.substring(0, mid).equals(str.substring(mid))) this.result+= left;
-//            left++;
-//        }
     }
 }
--- src/main/java/com/prakash/Main.java
+++ src/main/java/com/prakash/Main.java
@@ -6,7 +6,9 @@ public class Main {
         // Day1 day1 = new Day1();
         Day2 day2 = new Day2();
 
-        // day1.solve("input.txt");
-        day2.solve("input_day2.txt");
+//        day1.solve("input.txt");
+//        System.out.println(Long.MAX_VALUE);
+//        System.out.println(day2.validSeven("1011011"));
+        day2.solve("test_day2.txt");
     }
 }
